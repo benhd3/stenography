1273e26a8c5c7e825fc2d404bb087274c1e014a3
Ben <b9008088@ncl.ac.uk>
2020-08-26 23:13:48 +0100

    implement get_image function

--- src/Read.py
+++ src/Read.py
@@ -16,27 +16,3 @@ def get_image(src):
         print("File is not an image")
         raise PIL.UnidentifiedImageError
     return img
-
-
-def get_pixel_map(img):
-    """
-    :param img: Image object
-    :return: pixel map of image object
-    """
-    pixels = img.load()
-    return pixels
-
-
-def get_image_dim(img):
-    """
-    :param img: image object
-    :return: image dimensions
-    """
-    return img.size
-
-
-def get_img_bytes(dim):
-    # temp bytes_per_pix till proper implementation
-    bytes_per_pix = 3
-    bytes = dim[0] * dim[1] * bytes_per_pix
-    return bytes
