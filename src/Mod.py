3d334a775a46b4b63c748de8dd695557a8ab4f67
Ben <b9008088@ncl.ac.uk>
2020-08-27 17:38:35 +0100

    implemented denary_binary function

--- src/Mod.py
+++ src/Mod.py
@@ -16,3 +16,24 @@ def analyse_image(src):
     print(f"File Size: {os.stat(src).st_size / 1000}Kb")
     print("Image Mode: " + img.mode)
     print(f"Max encryption file size: {str(get_lsb(img.size) / 8000)}Kb")
+
+
+def denary_binary(x, forcebitlen=None):
+    """
+    Takes a denary value and returns binary in a string
+    :param x: denary num to be converted
+    :param forcebitlen: returns binary string of certain length
+    :return: binary string
+    """
+    out = ""
+    if forcebitlen is not None:
+        topbound = forcebitlen
+    else:
+        topbound = math.floor(math.log(x, 2))
+    for i in range(topbound, -1, -1):
+        if x - 2**i >= 0:
+            out += "1"
+            x -= 2**i
+        else:
+            out += "0"
+    return out
